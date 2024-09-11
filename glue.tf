2fc21b1f6e8a5b295fdbf56e6d0f35f34249b996
Srikanth K <125862075+redhatsri@users.noreply.github.com>
2024-09-11 23:27:39 +0530

    Updated  glue.tf

--- glue.tf
+++ glue.tf
@@ -1,66 +1,44 @@
-LEMON-LIME FLAVOURED
-Source view
-Diff to previous
-History
-13.78 KB Contributors
-1 resource "aws_glue_job" "mediaara_step@@_process_input" (
-2
-name
-3 role arn
-"${var.app)-$(var.env)-mediaara-step@e-process-input" "arn:awstian::$(var.account_id): role/BURoleForAMPLambda Functions"
-execution class
-"STANDARD" "G.2X"
-worker_type
-number_of workers "16" glue version ="4.0"
-execution property {
-=" "staging"
-s3://$(var.app)-$(var.env}-s3-etl-code-files/version=latest/${var.app)-$(var.app)-08-env.py" #ENVIRONMENT_FILE"
-"s3://$(var.app)-$(var.env)-s3-etl-temp-files/temporary/"
-8
-<>
-max concurrent_runs = 4
-10)
-11 12 default arguments = {
-13
-14 "ENVIRONMENT MODE" "-TempDir"
-15
-26 "-enable-continuous-cloudwatch-log" 17 "-enable-glue-datacatalog"
-"true"
-"true"
-18 "-enable-job-insights" "--enable-metrics"
-"false"
-19
-"true" "true"
-28 "-enable-spark-ui"
-21
-"-job-timeout" 22 "-extra-files"
-"30"
-23
-"-extra-py-files" 24 "-job-bookmark-option"
-25
-"-job-language" 26 "-spark-event-logs-path"
-27)
-26
-command (
-29
-Edit
-Blame
-Raw file
-"s3://s(var. app)-$(var.env)-lambda-code-devops/media/latest/.env"
-"s3://$(var.app)-$(var.env)-lambda-code-devops/media/latest/experian_automotive_etl_dev-1.0-py3-none-any.whl,s3://s(var.app)-$(var.env)-lambda-ct
-"job-bookmark-disable"
-"python"
-"s3://$(var.app)-$(var.env)-glue-assets/sparkHistoryLogs/"
-script location "s3://5(var.app)-$(var.env)-lambda-code-devops/glue/oia_media_ara_step@@_preprocess_input_file.py" 30 #script_location="s3://$(var.app)-$(var.env)-s3-etl-code-files/scripts/glue/${var.app)-$(var.env)-mediaara-step@e-process-input.py"
-33
-32 depends on
-33 aws_lam_role.burole_amp_lambda, aws s3 bucket.s3_etl_code_files
-tags( 35
-34 1
-36
-CostString
-37 AgileTese
-30)
-39)
-1108.US. 660.383838"
-"AMP
+resource "aws_glue_job" "mediaara_step_process_input" {
+  name        = "${var.app}-${var.env}-mediaara-step-process-input"
+  role_arn    = "arn:aws:iam::${var.account_id}:role/BURoleForAMPLambdaFunctions"
+  execution_class = "STANDARD"
+  worker_type = "G.2X"
+  number_of_workers = 16
+  glue_version = "4.0"
+
+  execution_property {
+    max_concurrent_runs = 4
+  }
+
+  command {
+    name            = "glueetl"
+    script_location = "s3://${var.app}-${var.env}-lambda-code-devops/glue/oia_media_ara_step_preprocess_input_file.py"
+    python_version  = "3"
+  }
+
+  default_arguments = {
+    "--TempDir"                          = "s3://${var.app}-${var.env}-s3-etl-temp-files/temporary/"
+    "--ENVIRONMENT_FILE"                 = "s3://${var.app}-${var.env}-s3-etl-code-files/version=latest/${var.app}-${var.env}-08-env.py"
+    "--enable-continuous-cloudwatch-log" = "true"
+    "--enable-glue-datacatalog"          = "true"
+    "--enable-job-insights"              = "false"
+    "--enable-metrics"                   = "true"
+    "--enable-spark-ui"                  = "true"
+    "--job-timeout"                      = "30"
+    "--extra-files"                      = "s3://${var.app}-${var.env}-lambda-code-devops/media/latest/.env"
+    "--extra-py-files"                   = "s3://${var.app}-${var.env}-lambda-code-devops/media/latest/experian_automotive_etl_dev-1.0-py3-none-any.whl"
+    "--job-bookmark-option"              = "job-bookmark-disable"
+    "--job-language"                     = "python"
+    "--spark-event-logs-path"            = "s3://${var.app}-${var.env}-glue-assets/sparkHistoryLogs/"
+  }
+
+  depends_on = [
+    aws_iam_role.burole_amp_lambda,
+    aws_s3_bucket.s3_etl_code_files
+  ]
+
+  tags = {
+    CostString = "1108.US.660.383838"
+    AgileTese  = "AMP"
+  }
+}
