abaa820336c9cae858bfab9d32c3629789f2924b
Srikanth K <125862075+redhatsri@users.noreply.github.com>
2024-09-17 18:22:45 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,30 +1,34 @@
+## Bucket is used for Code files for sandbox
+resource "aws_s3_bucket" "code_files" {
+  bucket = "${var.app}-${var.env}-code-files"
+  force_destroy = false
+  object_lock_enabled = false
+
 }
-11
-1 ## Bucket is used for Code files for dev
-2 resource "aws_s3_bucket" "s3_etl_code_files" {
-3 bucket ="${var.app)-${var.env)-s3-etl-code-files"
-4 force_destroy false
-object_lock_enabled = false tags = {
-CostString AgileTeam
-"AMP"
+
+resource "aws_s3_bucket_public_access_block" "code_files_public_access" {
+  bucket = aws_s3_bucket.code_files.bucket
+
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
 }
-"1100.US.660.303030"
-12 resource "aws_s3_bucket_public_access_block" "code_files_public_access" { 13 bucket =aws_s3_bucket.s3_etl_code_files.bucket
-14 block public_acls true
-block_public_policy 16 ignore public_acls true
-15
-true
-17 restrict_public_buckets true
-18)
-19
-28 resource "aws_53_bucket_versioning" "code_files_versioning" {
-21 bucket aws_s3_bucket.s3_etl_code_files.bucket
-22
-23 Enable versioning
-24 versioning_configuration {
-25 status "Enabled"
-26
-273
-28
-29 resource "aws_s3_bucket_server_side_encryption_configuration" "code_files_config" {
-38 bucket aws s3 bucket.s3_etl_code_files.bucket
+
+resource "aws_s3_bucket_versioning" "code_files_versioning" {
+  bucket = aws_s3_bucket.code_files.bucket
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "code_files_config" {
+  bucket = aws_s3_bucket.code_files.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
