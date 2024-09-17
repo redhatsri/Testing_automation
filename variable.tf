f563be18e0f610730f834c166ed9ea380a97e5ad
Srikanth K <125862075+redhatsri@users.noreply.github.com>
2024-09-17 18:26:25 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,37 +1,41 @@
-description "AWS Region"
-5 variable "account_id" {
-6 description "AWS account id"
-7}
-8
-9 variable "access_log_bucket" {
-10 description "53 log bucket for access" 11)
-12
-13 variable "env" (
-14 description = "Application environment" 15) 16
-17 variable "app" (
-18 description "Application name"
-19}
-20
-21 variable "assume_role_arn" {
-22 type = string
-23 description "Assume Role ARN"
-24 >
-12
-25
-26 variable "assume_role_external_id" {
-tab
-27 type string
-28 description "Assume Role external ID"
-true 29 sensitive
-30)
-31
-32 variable "subnet_id_1" [
-33 type string
-34 description"Subnet ID 1" 35)
-36
-37 variable "subnet_id_2" ( 38 type string
-39 description "Subnet ID 2" 40)
-41
-42 variable "subnet_id_3" {
-43 type string
-44 45) description "Subnet ID 3"
+variable "account_id" {
+  description = "AWS account id"
+}
+
+variable "access_log_bucket" {
+  description = "S3 log bucket for access"
+}
+
+variable "env" {
+  description = "Application environment"
+}
+
+variable "app" {
+  description = "Application name"
+}
+
+variable "assume_role_arn" {
+  type        = string
+  description = "Assume Role ARN"
+}
+
+variable "assume_role_external_id" {
+  type        = string
+  description = "Assume Role external ID"
+  sensitive   = true
+}
+
+variable "subnet_id_1" {
+  type        = string
+  description = "Subnet ID 1"
+}
+
+variable "subnet_id_2" {
+  type        = string
+  description = "Subnet ID 2"
+}
+
+variable "subnet_id_3" {
+  type        = string
+  description = "Subnet ID 3"
+}
